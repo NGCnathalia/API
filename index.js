c1a886bfc67452828460f685680461765a35aed2
NGCnathalia <nthaliagarcia@gmail.com>
2023-02-22 23:45:00 -0500

    Conection Database

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola tu.")
+require('dotenv').config()
+const exress = require('express')
+const cors = require('cors')
+const app = exress()
+const { dbConnect } = require('./config/mongo')
+
+const PORT = process.env.PORT || 3000
+app.use(cors())
+app.use(exress.json())
+
+// app.use('/api/1.0', require('./app/routes'))
+
+dbConnect()
+app.listen(PORT, () => {
+    console.log('API lista por el puerto ', PORT)
+})
